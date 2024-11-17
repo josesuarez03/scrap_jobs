8f19a032afb77ff1844b64efe7fcee42de94a6a4
Jose Suarez <josysuarez2003@gmail.com>
2024-11-17 15:26:56 +0100

    añadiendo las keywords para el scraper en la config

--- server/config.py
+++ server/config.py
@@ -7,4 +7,9 @@ class Config:
     MONGO_URI = os.getenv("MONGO_URI")
     DB_NAME = os.getenv("DB_NAME")
     SECRET_KEY = os.getenv("SECRET_KEY")
-    DEBUG = os.getenv("DEBUG", True)
+    KEYWORDS = [
+        "becario", "becarios", "prácticas", "practicas", "intern", "internship",
+        "informatica", "informática", "desarrollo", "programador", "programación",
+        "fullstack", "full stack", "software", "developer", "engineering",
+        "junior", "trainee", "estudiante", "recién graduado"
+    ]
