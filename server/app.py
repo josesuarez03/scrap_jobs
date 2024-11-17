33747782cd51167a96546579f4dcc4d3f196c5e2
Jose Suarez <josysuarez2003@gmail.com>
2024-11-17 15:54:50 +0100

    update: inicia la base de datos

--- server/app.py
+++ server/app.py
@@ -2,10 +2,18 @@ from flask import Flask
 from flask_cors import CORS
 from routes.routes import jobs_blueprint
 from config import Config
+from models.db_models import MongoDB
 
 app = Flask(__name__)
 CORS(app)
 app.config.from_object(Config)
+
+# Inicializar MongoDB
+mongodb = MongoDB()
+mongodb.init_app(app)
+
+# Hacer la instancia de db disponible globalmente
+db = mongodb.get_db()
 
 app.register_blueprint(jobs_blueprint)
 
